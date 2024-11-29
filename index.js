6d34ca07ce5964107a66cc9648fdc7ea888617a9
phpwechat <3257132998@qq.com>
2024-11-29 12:33:13 +0800

    Remove xml support for now

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { toJsonSchema } = require("json2json-schema"),  // Import the json2json-schema library to convert JSON to JSON Schema
     _ = require('lodash'),                            // Import lodash for utility functions
     JSON5 = require('json5'),                         // Import JSON5 to parse JSON5 formatted strings
-    xml2js = require('xml2js'),                       // Import xml2js to parse XML strings into JavaScript objects
+    // { js2xml, xml2js } = require('xml-js'),                       // Import xml2js to parse XML strings into JavaScript objects
     jsf = require('apipost-mock-schema'),             // Import apipost-mock-schema to generate mock data from a schema
     jsfObj = new jsf();                               // Create a new instance of the mock schema generator
 
@@ -11,43 +11,58 @@ module.exports = (str, ai = false) => {               // Export a module functio
             resolve(String(str))                      // Resolve the promise with the string representation of the input
         }
 
-        xml2js.parseString(str, (err, result) => {    // Attempt to parse the input as XML
-            if (err) {                                // If parsing fails, handle as potential JSON
-                try {
-                    const json = JSON5.parse(str);    // Parse the string as JSON5
-                    try {
-                        const schema = toJsonSchema(json);           // Convert JSON to JSON Schema
-                        jsfObj.mock(schema).then(res => {            // Generate mock data from the schema
-                            resolve(JSON.stringify(res, null, "\t")); // Resolve the promise with the mock data as a pretty-printed JSON
-                        }).catch(err => {
-                            resolve(str)                              // If mock generation fails, resolve with original input
-                        })
-                    } catch (err) {
-                        resolve(str)                                  // If schema conversion fails, resolve with original input
-                    }
-                } catch (err) {
-                    resolve(str)                                      // If JSON5 parsing fails, resolve with original input
-                }
-            } else { // xml
-                if (_.isPlainObject(result) && !_.isEmpty(result)) {  // Check if parsed XML result is a non-empty object
-                    try {
-                        const schema = toJsonSchema(result);          // Convert XML result to JSON Schema
-                        jsfObj.mock(schema).then(res => {             // Generate mock data from the schema
-                            const { js2xml } = require('xml-js');     // Import xml-js to convert JavaScript objects to XML
-                            try {
-                                const xml = js2xml(res, { compact: true, spaces: 4 });  // Convert mock data back to XML
-                                resolve(xml);                             // Resolve the promise with the generated XML
-                            } catch (err) {
-                                resolve(str)                              // If XML conversion fails, resolve with original input
-                            }
-                        }).catch(err => {
-                            resolve(str)                                  // If mock generation fails, resolve with original input
-                        })
-                    } catch (err) {
-                        resolve(str)                                      // If schema conversion fails, resolve with original input
-                    }
-                }
+        try {
+            const json = JSON5.parse(str);    // Parse the string as JSON5
+            try {
+                const schema = toJsonSchema(json);           // Convert JSON to JSON Schema
+                jsfObj.mock(schema).then(res => {            // Generate mock data from the schema
+                    resolve(JSON.stringify(res, null, "\t")); // Resolve the promise with the mock data as a pretty-printed JSON
+                }).catch(err => {
+                    resolve(str)                              // If mock generation fails, resolve with original input
+                })
+            } catch (err) {
+                resolve(str)                                  // If schema conversion fails, resolve with original input
             }
-        });
+        } catch (err) {
+            resolve(str)                                      // If JSON5 parsing fails, resolve with original input
+        }
+        // xml2js.parseString(str, (err, result) => {    // Attempt to parse the input as XML
+        //     if (err) {                                // If parsing fails, handle as potential JSON
+        //         try {
+        //             const json = JSON5.parse(str);    // Parse the string as JSON5
+        //             try {
+        //                 const schema = toJsonSchema(json);           // Convert JSON to JSON Schema
+        //                 jsfObj.mock(schema).then(res => {            // Generate mock data from the schema
+        //                     resolve(JSON.stringify(res, null, "\t")); // Resolve the promise with the mock data as a pretty-printed JSON
+        //                 }).catch(err => {
+        //                     resolve(str)                              // If mock generation fails, resolve with original input
+        //                 })
+        //             } catch (err) {
+        //                 resolve(str)                                  // If schema conversion fails, resolve with original input
+        //             }
+        //         } catch (err) {
+        //             resolve(str)                                      // If JSON5 parsing fails, resolve with original input
+        //         }
+        //     } else { // xml
+        //         if (_.isPlainObject(result) && !_.isEmpty(result)) {  // Check if parsed XML result is a non-empty object
+        //             try {
+        //                 const schema = toJsonSchema(result);          // Convert XML result to JSON Schema
+        //                 jsfObj.mock(schema).then(res => {             // Generate mock data from the schema
+        //                     const { js2xml } = require('xml-js');     // Import xml-js to convert JavaScript objects to XML
+        //                     try {
+        //                         const xml = js2xml(res, { compact: true, spaces: 4 });  // Convert mock data back to XML
+        //                         resolve(xml);                             // Resolve the promise with the generated XML
+        //                     } catch (err) {
+        //                         resolve(str)                              // If XML conversion fails, resolve with original input
+        //                     }
+        //                 }).catch(err => {
+        //                     resolve(str)                                  // If mock generation fails, resolve with original input
+        //                 })
+        //             } catch (err) {
+        //                 resolve(str)                                      // If schema conversion fails, resolve with original input
+        //             }
+        //         }
+        //     }
+        // });
     })
 }
